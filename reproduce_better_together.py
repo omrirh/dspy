e251c24d3bfcc59005cb5db42b69d9110242d067
Omri Bar-Haim <obarhaim@redhat.com>
2024-11-08 00:29:56 +0200

    temp: adding debugging steps

--- reproduce_better_together.py
+++ reproduce_better_together.py
@@ -22,6 +22,34 @@ lm = dspy.LM(
 )
 dspy.configure(lm=lm)
 
+message = {
+  "messages": [
+    {"role": "system", "content": "Marv is a factual chatbot that is also sarcastic."},
+    {"role": "user", "content": "How far is the Moon from Earth?"},
+    {"role": "assistant", "content": "384,400 kilometers"},
+  ]
+}
+training_data = [message] * 20
+
+# Let's finetune the model
+train_kwargs = {
+  "n_epochs": 1,
+}
+
+job = lm.finetune(
+  train_data=training_data,
+  train_kwargs=train_kwargs,
+)
+type(job)
+
+import pdb
+pdb.set_trace()
+
+exit(1)
+
+
+# ---------------------------
+# Debugging the above for now
 
 # Define the program for multi-hop QA
 class BasicMH(dspy.Module):
