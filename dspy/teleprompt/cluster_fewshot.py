d66d19dcf94208f7b5e1e10c1f8ca0e6adc3631d
Omri Bar-Haim <obarhaim@redhat.com>
2025-03-21 20:24:21 +0200

    log changes to ClusterFewshot, import fix on EvaluateEntropy

--- dspy/teleprompt/cluster_fewshot.py
+++ dspy/teleprompt/cluster_fewshot.py
@@ -1,8 +1,8 @@
 import logging
 import numpy as np
+from typing import List
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
-from typing import List, Dict, Optional
 from sklearn.cluster import KMeans
 from dspy.primitives import Program, Example
 from sentence_transformers import SentenceTransformer
