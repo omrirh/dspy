4e0d671087f414ec11d2ee6a784822e8cf4f6db6
Omri Bar-Haim <obarhaim@redhat.com>
2025-12-18 04:48:27 +0200

    revert reflector minibatch size for GEPA

--- better_together_experiment.py
+++ better_together_experiment.py
@@ -144,7 +144,6 @@ def main(dataset, prompt_optimizer, strategy, model):
             auto="light",
             reflection_lm=lm,
             instruction_proposer=None,
-            reflection_minibatch_size=10,
             num_threads=6,
         )
 
